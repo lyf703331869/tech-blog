d41f948529327bc8ebc5f2d0ff0c03d9e578067f
Yafei Liu <yafeiliu@yafeis-air.mynetworksettings.com>
2023-08-16 11:40:53 -0400

    Added typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,50 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    blogs: [Blog]!
+  }
+
+  type Blog {
+    _id: ID
+    blogText: String
+    blogAuthor: String
+    createdAt: String
+    comments: [Comment]!
+  }
+
+  type Comment {
+    _id: ID
+    commentText: String
+    commentAutor: String
+    createdAt: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    blogs(username: String!): [Blog]
+    blog(blogId: ID!): Blog
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addBlog(blogText: String!): Blog
+    addComment(blogId: ID!, commentText: String!): Blog
+    removeBlog(blogId: ID!): Blog
+    removeComment(blogId: ID!, commentId: ID!): Blog
+  }
+`;
+
+module.exports = typeDefs;
